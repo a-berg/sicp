37c518b77d9f87f511460e7f52eebd50894e7aaa
xylon <xylon.beta@gmail.com>
2020-03-18 10:36:50 +0100

    Redid exercise 1-12 in org, then tangled it.
    I think it will be a good thing to do exercises in org-mode and execute/tangle
    them, as literate programming helps with reasoning. Might as well remake some of
    the other exercises (especially 1-10 and 1-11) in org too.

--- chapter1/1-12.rkt
+++ chapter1/1-12.rkt
@@ -1,9 +1,6 @@
 #lang sicp
 (#%require rackunit)
 
-;; Given an n (level) and an m (position in that level), compute Pascal's
-;; triangle.
-;; I don't check for bounds.
 (define (PT n m)
   (cond ((< n 2) 1)
         ((= m 0) 1)
@@ -14,6 +11,3 @@
 (check-equal? (PT 3 2) 3)
 (check-equal? (PT 5 2) 10)
 (check-equal? (PT 5 4) 5)
-
-;; Improvement here:
-
